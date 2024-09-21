28b2173834763d92348d9e6f3b05972e958275bd
shivanand-k <shivanandcrew034@gmail.com>
2024-09-21 17:38:18 +0530

    changes are made in email templating

--- server/utils/mailSending.js
+++ server/utils/mailSending.js
@@ -14,7 +14,24 @@ exports.sendConfirmationMail = async(userEmail , organizationMail , jobTitle , u
         from:"natsudragneel771990@gmail.com",
         to:userEmail,
         subject:"Application is submitted",
-        text:"your application is submitted to organization you will shortly receieve official meesage",
+        text:`Dear [Job Seeker's Name],
+        Thank you for applying to the ${jobTitle} position at [Company Name]. We have successfully received your application.
+        Here are the details of your application:
+        - Position: ${jobTitle}
+        - Company: [Company Name]
+        - Submitted On: [Submission Date]
+        - Job Location: [Job Location]
+
+        Our hiring team is reviewing your profile and will contact you shortly if we feel you are a good match for the role. In the meantime, you can visit your profile to track the status of your application.
+
+        If you have any questions or need to update your information, feel free to contact us at [natsudragneel771990@gmail.com].
+
+        Thank you again for your interest in joining our team, and we wish you the best of luck in the hiring process!
+
+        Best regards,  
+        [Job-DashBoard]  
+        [Company Website]  
+        [natsudragneel771990@gmail.com]`,
     };
     let resumeBuffer;
     try {
@@ -28,7 +45,26 @@ exports.sendConfirmationMail = async(userEmail , organizationMail , jobTitle , u
         from:"natsudragneel771990@gmail.com",
         to:organizationMail,
         subject:`new application is received `,
-        text:`you have received the new job application for the job ${jobTitle}`,
+        text:`Dear [Employer's Name],
+
+        We wanted to inform you that a new application has been submitted for your job posting: [Job Title].
+
+        Applicant details:
+        - Name: [Job Seeker's Name]
+        - Email: ${userEmail}    
+        - Applied On: [Submission Date]
+        - Resume: Link of Resume is Below
+
+        You can review the full application on the employer dashboard by following this link: [Dashboard Link].
+
+        If you have any questions or need assistance reviewing applications, please feel free to contact us at [natsudragneel771990@gmail.com].
+
+        Thank you for using [Your Platform Name] to find your next great hire.
+
+        Best regards,  
+        Job-DashBoard  
+        [Company Website]  
+        natsudragneel771990@gmail.com`,
         html:"<h1>applied user resume is</h1>",
         attachments:[
             {
